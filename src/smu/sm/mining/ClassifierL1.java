24e4cf13d59f190ee6849cfc415257668bfea162
trongnt89 <trongnt.cs@gmail.com>
2015-03-27 20:15:22 +0800

    classifiers

---
+++ src/smu/sm/mining/ClassifierL1.java
@@ -0,0 +1,10 @@
+package smu.sm.mining;
+
+public class ClassifierL1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/smu/sm/mining/ClassifierL2.java
@@ -0,0 +1,10 @@
+package smu.sm.mining;
+
+public class ClassifierL2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
